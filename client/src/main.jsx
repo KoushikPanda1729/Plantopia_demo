e89f5f28e3a5d59e3f5528b488d505071f42b48f
Git_Koushik <programmer747767@gmail.com>
2024-08-16 14:56:49 +0530

    google client id updaetd

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const googleClientId = import.meta.env.VITE_GOOGLE_SIGNUP_CLIENT_ID;
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
