b4c3a573845c7a04512daab2dedf077c2b5c608b
Git_Koushik <programmer747767@gmail.com>
2024-08-16 17:42:38 +0530

    config

--- server/src/index.js
+++ server/src/index.js
@@ -9,11 +9,9 @@ connectDB()
       console.log("Error occured after DB connection : ", error);
     });
 
-    // app.listen(process.env.PORT || 8000, () => {
-    //   console.log("App is running at port : ", process.env.PORT);
-    // });
-
-    app(req, res); // Use Express to handle the request
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("App is running at port : ", process.env.PORT);
+    });
   })
   .catch((error) => {
     console.log("MongoDB connection error : ", error);
