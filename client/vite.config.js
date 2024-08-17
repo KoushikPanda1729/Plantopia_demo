731914acd91f65775df696ed86d64bcedc136a7c
Git_Koushik <programmer747767@gmail.com>
2024-08-17 09:09:06 +0530

    con

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
+      // "/api": "http://localhost:8000",
       // "/api": process.env.VITE_API_BASE_URL,
-      // "/api": "https://plantopia-demo.vercel.app",
+      "/api": "https://plantopia-demo.vercel.app",
     },
   },
   plugins: [react()],
